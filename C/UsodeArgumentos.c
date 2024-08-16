a42cfaf2307337a94ce80ddabead0242cfa675e8
Daniel Portella <68924608+DPortella@users.noreply.github.com>
2024-08-16 19:10:41 -0300

    Atualizando com ? :

---
+++ C/UsodeArgumentos.c
@@ -0,0 +1,16 @@
+#include <stdio.h>        // Argumentos
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+    int mes;
+    char *nomemes[] = {"Janeiro", "Fevereiro", "Marco", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro",
+    "Novembro", "Dezembro"};
+
+    if(argc == 4){
+        mes = atoi(argv[2]);
+        mes<1 || mes>12 ? printf("Erro, mes invalido"): printf("\n%s de %s de 20%s \n", argv[1], nomemes[mes -1], argv[3]);
+    }
+    else
+        printf("Erro! Os numeros devem ser inteiros!");
+}
