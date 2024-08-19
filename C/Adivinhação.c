874fce7838075d9d1e32097b16656ed083b1e257
Daniel Portella <68924608+DPortella@users.noreply.github.com>
2024-08-19 16:41:35 -0300

    Create Adivinhação.c

---
+++ C/Adivinhação.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void main(void){
+int num;
+int adv;
+srand(time(NULL));
+
+adv = rand();
+
+while(adv != num){
+
+printf("Adivinhe o numero:\n");
+scanf("%d", &num);
+
+if(num != adv){
+    if(num <= adv)
+        printf("Errou! Tente um numero maior\n");
+    else
+        printf("Errou! Tente um numero menor\n");
+}
+else printf("***CERTO!***");
+}
+}
