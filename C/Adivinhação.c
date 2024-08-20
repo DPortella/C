837c2bdac7a8791e320cc803fcd3f2b4e56aef35
Daniel Portella <68924608+DPortella@users.noreply.github.com>
2024-08-20 13:49:59 -0300

    Update Adivinhação.c

--- C/Adivinhação.c
+++ C/Adivinhação.c
@@ -15,10 +15,7 @@ printf("Adivinhe o numero:\n");
 scanf("%d", &num);
 
 if(num != adv){
-    if(num <= adv)
-        printf("Errou! Tente um numero maior\n\n");
-    else
-        printf("Errou! Tente um numero menor\n\n");
+    num <= adv ? printf("Errou! Tente um numero maior\n\n"): printf("Errou! Tente um numero menor\n\n");
 }
 else printf("***CERTO!***");
 }
