340772a5206bdab5c047bba6e1698262aad9477e
Daniel Portella <68924608+DPortella@users.noreply.github.com>
2024-08-20 17:42:22 -0300

    Update Fatorial.c

--- C/Fatorial.c
+++ C/Fatorial.c
@@ -3,11 +3,8 @@ int main(int argc, char *argv[]){
     int fact=1, i, num;
     if(argc == 2){
         num = atoi(argv[1]);
-        for(i = 1; i<=num; i++){
-            fact *= i;
-        }
+        for(i = 1; i<=num; i++)  fact *= i;
         printf("%d", fact);
       }  else
         printf("Erro");
-
     }
