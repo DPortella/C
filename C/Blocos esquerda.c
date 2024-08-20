42a8e0bbe17e914476e677abc55c6c7b557670bb
Daniel Portella <68924608+DPortella@users.noreply.github.com>
2024-08-20 17:46:27 -0300

    Update Blocos esquerda.c

--- C/Blocos esquerda.c
+++ C/Blocos esquerda.c
@@ -1,24 +1,18 @@
-#include <stdio.h>        // Blocos à Esquerda
+#include <stdio.h>        // Blocos Ã  Esquerda
 void print(char c, int n);
 
 int main(void){
     int n;
-
-        printf("Quantos blocos?\n");
-        scanf("%d", &n);
-
+    printf("Quantos blocos?\n");
+    scanf("%d", &n);
 
     for (int i = 0; i < n; i++){
             print('#', i + 1);
             printf("\n");
-
         }
 }
+
 void print(char c, int n){
 
-    for (int i = 0; i < n; i++){
-
-        printf ("%c", c);
-    }
+    for (int i = 0; i < n; i++)printf ("%c", c);
 }
-
