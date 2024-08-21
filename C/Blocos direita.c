7df677c742b04e3b30e8af296043b4a752c60105
Daniel Portella <68924608+DPortella@users.noreply.github.com>
2024-08-21 13:06:15 -0300

    Update Blocos direita.c

--- C/Blocos direita.c
+++ C/Blocos direita.c
@@ -1,10 +1,10 @@
 #include <stdio.h>      //Blocos à direita
 int main(){
-    int blocos;
+    int blocos, i = 0;
     printf("Quantos blocos?\n");
     scanf("%d", &blocos);
 
-    for(int i=0; i<blocos; i++){
+    for( ; i<blocos; i++){
             for(int j=0; j<blocos; j++) i+j < blocos-1 ? printf(" ") : printf("#");
             printf("\n");
     }
