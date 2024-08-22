ea980f4eae300faa043c2a9158868b739dddb578
Daniel Portella <68924608+DPortella@users.noreply.github.com>
2024-08-22 18:52:14 -0300

    Create Matriz_bidimensional.c

---
+++ C/Matriz_bidimensional.c
@@ -0,0 +1,14 @@
+#include <stdio.h>      //Matriz bidimensional
+
+void main(void){
+int t, i, num[3][4];
+for(t=0; t<3; t++)
+    for(i=0; i<4; i++)
+     num[t][i] = (t*4)+i+1;
+
+for(t=0; t<3; t++){
+    for(i=0; i<4; i++)
+        printf("%d", num [t][i]);
+    printf("\n");
+}
+}
