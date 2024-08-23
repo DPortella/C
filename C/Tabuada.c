bd81d87d0e5c428441f88b0fc9e6013c4c59a70e
Daniel Portella <68924608+DPortella@users.noreply.github.com>
2024-08-23 16:51:36 -0300

    Update Tabuada.c

--- C/Tabuada.c
+++ C/Tabuada.c
@@ -14,6 +14,6 @@ char cod;
         for(i = 1; i <= vezes; i++) printf("%d x %d = %d\n", numero, i, numero*i);
 
         printf("Deseja uma nova operacao? Digite 'S' ou 'N'\n");
-        cod = getche();
+        cod = toupper(getche());
     }
 }
