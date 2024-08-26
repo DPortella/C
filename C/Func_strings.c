70d5c83de120c0b73ae920a3cb94832d12fa7ea2
Daniel Portella <68924608+DPortella@users.noreply.github.com>
2024-08-26 18:53:56 -0300

    Create Func_strings.c

---
+++ C/Func_strings.c
@@ -0,0 +1,18 @@
+#include <stdio.h>  //Funcs de strings
+#include <string.h>
+void main(void){
+char s1[80], s2[80];
+gets(s1);
+gets(s2);
+
+printf("Comprimentos: %d %d\n", strlen(s1), strlen(s2));
+
+if(!strcmp(s1, s2)) printf("As strings sao iguais\n");
+strcat(s1, s2);
+printf("%s\n", s1);
+
+strcpy(s1, "Isso e um teste\n");
+printf(s1);
+if(strchr("alo", 'o')) printf("o esta em alo\n");
+if(strstr("ola aqui", "ola")) printf("ola encontrado");
+}
