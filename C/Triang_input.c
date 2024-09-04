1cbd02fc781dba384cc65785e38f8cdad42401cf
Daniel Portella <68924608+DPortella@users.noreply.github.com>
2024-09-04 18:33:51 -0300

    Create Triang_input.c

---
+++ C/Triang_input.c
@@ -0,0 +1,21 @@
+#include <stdio.h>        // Triangulos
+#include <stdlib.h>
+
+int main(void){
+    float lado[3];
+    int i;
+
+    for(i=0; i<3; i++){
+        printf("Indique o valor do lado %d:\n", i+1);
+        scanf("%f", &lado[i]);
+    }
+        if(lado[0] + lado[1] < lado[2] || lado[0] + lado[2] < lado[1] || lado[2] + lado[1] < lado[0])
+            printf("Isso nao eh um triangulo");
+        else
+            if(lado[0] == lado[1] && lado[0] == lado[2])
+                printf("Triangulo equilatero.");
+            else
+                lado[0] != lado[1] && lado[0] != lado[2] && lado[1] != lado[2] ? printf("Triangulo escaleno."): printf("Triangulo isosceles.");
+
+    return 0;
+}
