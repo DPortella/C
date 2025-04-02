fc39a7b05e87f0c2508902761808e23c954af9e0
Daniel Portella <68924608+DPortella@users.noreply.github.com>
2025-04-01 22:27:44 -0300

    Update Range.c

--- Range.c
+++ Range.c
@@ -1,27 +1,55 @@
 #include<stdio.h>
 int sigint();
 int unint();
+char sichar();
+unsigned char unchar();
+short sishort();
+short unshort();
 
+
+char sichar(){
+    return 1 << (sizeof(char) * 8 - 1);
+}
+
+unsigned char unchar(){
+    return (1 << (sizeof(char) * 8)) - 1;
+}
 
 int sigint(){
-        return 1 << (sizeof(int) * 8 - 1);
+    return 1 << (sizeof(int) * 8 - 1);
     }
 
 int unint(){
-        return 1 << (sizeof(int) * 8 -  1);
-
+    return (1 << (sizeof(int) * 8)) - 1;
     }
 
+short sishort(){
+    return 1 << (sizeof(short) * 8 -  1);
+}
+
+short unshort(){
+    return (1 << (sizeof(short) * 8)) - 1;
+}
 
 int main(){
 
-        int c = sigint();
-        printf("O signed int vai de %d ate %d\n", c, c - 1);
+    int c = sigint();
+    printf("O signed int vai de %d ate %d\n", c, ~c);
 
+    c = unint();
+    printf("O unsigned int vai de 0 ate %u\n", c);
 
-        c= unint();
-        printf("O unsigned int vai de 0 ate %u\n", (c*2) - 1);
+    c = sichar();
+    printf("O signed char vai de %d ate %d\n", c, ~c);
 
+    c = unchar();
+    printf("O unsigned char vai de 0 ate %u\n", c);
 
-        return 0;
+    c = sishort();
+    printf("O signed short vai de %d ate %d\n", c, ~c);
+
+    c = unshort();
+    printf("O unsigned short vai de 0 ate %hu\n", c);
+
+    return 0;
 }
